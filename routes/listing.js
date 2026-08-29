0c279031869f28e321daad0bda60efc3984f476a
Bedanand Kumar Sah <beducode12@gmail.com>
2026-08-29 14:50:47 +0530

    Add Initial Files

---
+++ routes/listing.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+// Index Route
+// router
+//     .route("/")
+//     .get(wrapAsync(listingController.index))
+//     .post(
+//         isLoggedIn,
+//         validateListing,
+//         wrapAsync(listingController.createListing)
+//     );
+
+
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))
+    .post(
+        isLoggedIn,
+        upload.single("listing[image]"),
+        validateListing,
+        wrapAsync(listingController.createListing)
+    );
+
+// New Route
+router.get(
+    "/new",
+    isLoggedIn,
+    listingController.renderNewForm
+);
+
+// Show + Edit + Update + Delete Routes
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(
+        isLoggedIn,
+        isOwner,
+        upload.single("listing[image]"),
+        validateListing,
+        wrapAsync(listingController.updateListing)
+    )
+    .delete(
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.destroyListing)
+    );
+
+// Edit Form Route
+router.get(
+    "/:id/edit",
+    isLoggedIn,
+    isOwner,
+    wrapAsync(listingController.renderEditForm)
+);
+
+module.exports = router;
